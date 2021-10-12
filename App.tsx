987396743616840f54155c19c3221ea441375546
Alberto <agonzalez@tribalyte.com>
2021-10-12 23:14:03 +0200

    (yarn install) Add home screen, navigation and visual dependencies
    - Add home screen, navigation and visual dependencies

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 
 import store from "./store/config";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={styles.container}>
-                <Text>Uber!</Text>
+            <SafeAreaProvider>
                 <StatusBar style="auto" />
-            </View>
+                <HomeScreen />
+            </SafeAreaProvider>
         </Provider>
     );
 }
